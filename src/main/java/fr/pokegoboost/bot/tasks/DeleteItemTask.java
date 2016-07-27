197ffe740989b3202618d6e6d56cb1e06273d5dc
Charles Blancheton <charles.blancheton@etu.u-bordeaux.fr>
2016-07-28 00:42:13 +0200

    add empty tasks

---
+++ src/main/java/fr/pokegoboost/bot/tasks/DeleteItemTask.java
@@ -0,0 +1,13 @@
+package fr.pokegoboost.bot.tasks;
+
+import fr.pokegoboost.bot.PokeBot;
+
+public class DeleteItemTask implements ITask{
+
+	@Override
+	public Object execute(PokeBot instance, Object... inputs) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
